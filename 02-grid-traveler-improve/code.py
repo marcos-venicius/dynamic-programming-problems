99acccb248113a52dd42d3e18c7b8736cf644037
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-02-25 13:50:04 -0300

    02-grid-traveler-improve key as m * n

--- 02-grid-traveler-improve/code.py
+++ 02-grid-traveler-improve/code.py
@@ -17,11 +17,34 @@ def traveler(rows, cols, memo = {}):
     down = traveler(rows - 1, cols, memo)
     right = traveler(rows, cols - 1, memo)
 
-    
     memo[f'{rows - 1}x{cols}'] = down
     memo[f'{rows}x{cols - 1}'] = right
 
     return down + right
+
+def traveler2(rows, cols, memo = {}):
+    if rows == 0 or cols == 0:
+        return 0
+
+    if cols == 1 and rows == 1:
+        return 1
+
+    key = rows * cols
+
+    if key  in memo:
+        return memo[key]
+
+    down = traveler(rows - 1, cols, memo)
+    right = traveler(rows, cols - 1, memo)
+
+    downKey = (rows - 1) * cols
+    rightKey = rows * (cols - 1)
+    
+    memo[key] = down + right
+    memo[downKey] = down
+    memo[rightKey] = right
+
+    return memo[key]
 
 tests = [
     ((1, 1), 1),
@@ -47,4 +70,13 @@ def test1():
         r = traveler(g[0], g[1])
         assert r == test[1], f"result should be {test[1]} (!= {r})"
 
+def test2():
+    print('using recursion')
+
+    for test in tests:
+        g = test[0]
+        r = traveler2(g[0], g[1])
+        assert r == test[1], f"result should be {test[1]} (!= {r})"
+
 duration(test1)
+duration(test2)
