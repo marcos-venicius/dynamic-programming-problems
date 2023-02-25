7618d52932e5a6a95596e3b70e4127adcec77af3
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-02-25 13:35:51 -0300

    02-grid-traveler-improve

---
+++ 02-grid-traveler-improve/code.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from time import time
+
+def traveler(rows, cols, memo = {}):
+    if rows == 0 or cols == 0:
+        return 0
+
+    if cols == 1 and rows == 1:
+        return 1
+
+    key = f'{rows}x{cols}'
+
+    if key  in memo:
+        return memo[key]
+
+    down = traveler(rows - 1, cols, memo)
+    right = traveler(rows, cols - 1, memo)
+
+    
+    memo[f'{rows - 1}x{cols}'] = down
+    memo[f'{rows}x{cols - 1}'] = right
+
+    return down + right
+
+tests = [
+    ((1, 1), 1),
+    ((2, 3), 3),
+    ((3, 3), 6),
+    ((18, 18), 2333606220) # it takes a long time
+]
+
+def duration(fn):
+    start = time()
+    try:
+        fn()
+    finally:
+        end = time()
+        d = end - start
+        print(f'duration: {d:.5f}s')
+
+def test1():
+    print('using recursion')
+
+    for test in tests:
+        g = test[0]
+        r = traveler(g[0], g[1])
+        assert r == test[1], f"result should be {test[1]} (!= {r})"
+
+duration(test1)
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Solve problems in dynamic programming way
 - Fibonnaci
   - [first try](./01-fibonacci/description.txt)
   - [improved way](./01-fibonacci-improve/description.txt)
+
+- Grid 2D traveler
+  - [first try](./02-grid-traveler/description.txt)
+  - [improved way](./02-grid-traveler-improve/description.txt)
